df7e849caf1dd260ab462186b751686404612db3
Richard Vergis <RichardTerminal@Richard.local>
2017-04-03 13:51:37 +0800

    first commit for Full MEAN Stack from scratch, development stage

---
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('You are logout.', { cssClass: 'alert-success', timeout: 3000 });
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+}
